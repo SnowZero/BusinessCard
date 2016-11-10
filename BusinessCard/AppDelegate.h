42d36f58573918f8a14f038739da49bbcb187f02
Snow <wl02475350@gmail.com>
2016-11-10 14:24:50 +0800

    first commit

---
+++ BusinessCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BusinessCard
+//
+//  Created by Snos on 2016/11/10.
+//  Copyright © 2016年 Snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BusinessCard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BusinessCard
+//
+//  Created by Snos on 2016/11/10.
+//  Copyright © 2016年 Snow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
